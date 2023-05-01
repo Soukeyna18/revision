6b7e13a1a098b2c6d87e057c0f549d8d24e95755
ndeye <ndeya17041998@gmail.com>
2023-05-01 19:13:08 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function changeImage() {
+  var image = document.getElementById("myImage");
+  if (image.src.match("image1.jpg")) {
+    image.src = "image2.jpg";
+  } else {
+    image.src = "image1.jpg";
+  }
+}
+
